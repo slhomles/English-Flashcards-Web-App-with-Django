607647d0980260ddac032eb2a5b445b36014fa62
Btl Python <letrungphuc311291@gmail.com>
2024-10-07 21:36:45 +0700

    Create topic and flashcard models

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    id_topic = models.IntegerField(primary_key = True, null = False)
+    name_topic = models.CharField(max_length = 50, blank = False)
+
+class Flashcards(models.Model):
+    id_flashcard = models.IntegerField(primary_key= True, null = False)
+    front = models.CharField(max_length = 200, blank = False)
+    back = models.CharField(max_length = 300, blank = False)
+    id_topic = models.ForeignKey(Topic,on_delete = models.CASCADE )
