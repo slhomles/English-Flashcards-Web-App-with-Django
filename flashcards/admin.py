cdcdd53c6fe9a4dd77151582de301a2f373cf71a
Btl Python <letrungphuc311291@gmail.com>
2024-10-08 16:27:36 +0700

    Changed admin model interface

--- flashcards/admin.py
+++ flashcards/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Topic, Flashcards
 
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id_topic','name_topic')
+
+class FlashcardsAdmin(admin.ModelAdmin):
+    list_display = ('id_flashcard','front','back')
+
 # Register your models here.
-admin.site.register(Topic)
-admin.site.register(Flashcards)
+admin.site.register(Topic,TopicAdmin)
+admin.site.register(Flashcards,FlashcardsAdmin)
--- flashcards/models.py
+++ flashcards/models.py
@@ -5,8 +5,14 @@ class Topic(models.Model):
     id_topic = models.IntegerField(primary_key = True, null = False)
     name_topic = models.CharField(max_length = 50, blank = False)
 
+    def __str__(self):
+        return f'{self.name_topic}'
+
 class Flashcards(models.Model):
     id_flashcard = models.IntegerField(primary_key= True, null = False)
     front = models.CharField(max_length = 200, blank = False)
     back = models.CharField(max_length = 300, blank = False)
     id_topic = models.ForeignKey(Topic,on_delete = models.CASCADE )
+
+    def __str__(self):
+        return f'{self.front}'
