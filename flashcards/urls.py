1f9f85e70163c2d12b017547c0ba71bb417bbc61
vth <thanhhuyenvo00@gmail.com>
2024-11-20 16:17:59 +0700

    them mot so topic va flashcard

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
+    path('', views.topics, name='topics'),
     path('topics/', views.topics, name='topics'),
     path('flashcards/<slug:slug_topic>/', views.flashcards, name='flashcards'),
     path('word_detail/<slug:slug_flashcard>/', views.word_detail, name='word_detail'),
